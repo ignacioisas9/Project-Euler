e100fdf1b75f59707c5a9100b9596fea46a66bbd
Ignacio <ignacioisas8@gmail.com>
2024-04-22 02:53:40 +0100

    problem31 unsolved

--- problem31.js
+++ problem31.js
@@ -4,8 +4,44 @@
 // 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
 // How many different ways can £2 be made using any number of coins?
 
+const total = 200
+
+// for 5 pences:
+// 5
+// 2 + 2 + 1
+// 2 + 1 + 1 + 1
+// 1 + 1 + 1 + 1 + 1
+// 4 ways
+
+// for 10 pences:
+// 10
+// 5 + 5
+// 5 + 2 + 2 + 1
+// 5 + 2 + 1 + 1 + 1
+// 5 + 1 + 1 + 1 + 1 + 1
+// 2 + 2 + 2 + 2 + 2
+// 2 + 2 + 2 + 2 + 1 + 1
+// 2 + 2 + 2 + 1 + 1 + 1 + 1
+// 2 + 2 + 1 + 1 + 1 + 1 + 1 + 1
+// 2 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+// 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+// 11 ways
+
 function waysOfMakingTwoPounds(){
-    return 
+    let ways = 1
+    const coins = [200, 100, 50, 20, 10, 5, 2, 1]
+    for (let coin = 1; coin < coins.length; coin++){
+        for (let i = 1; i < coins.length; i++){
+            let sum = coins[coin]
+            if (sum < total){
+                sum += coins[i]
+            } 
+            if (sum === total){
+                ways++
+            }
+        }
+    }
+    return ways
 }
 
 console.log(waysOfMakingTwoPounds())
